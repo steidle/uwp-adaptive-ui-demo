db41d44f14447cffeda19c3298690fb686fd5057
Patrick Dehne <patrick@mysonicweb.de>
2016-06-07 17:12:03 +0200

    Initial commit

---
+++ ContinuumTrigger.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaptiveUI
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Windows.UI.Xaml;
+
+    // Source:
+    // http://social.technet.microsoft.com/wiki/contents/articles/31069.windows-10-apps-leverage-continuum-feature-to-change-ui-for-mousekeyboard-users-using-custom-statetrigger.aspx
+
+    public class ContinuumTrigger : StateTriggerBase
+    {
+        public string UIMode
+        {
+            get { return (string)GetValue(UIModeProperty); }
+            set { SetValue(UIModeProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for UIMode.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty UIModeProperty =
+            DependencyProperty.Register("UIMode", typeof(string), typeof(ContinuumTrigger), new PropertyMetadata(""));
+
+        public ContinuumTrigger()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            if (!Windows.ApplicationModel.DesignMode.DesignModeEnabled)
+            {
+                WindowActivatedEventHandler windowactivated = null;
+                windowactivated = (s, e) =>
+                {
+                    Windows.UI.Xaml.Window.Current.Activated -= windowactivated;
+
+                    var currentUIMode = Windows.UI.ViewManagement.UIViewSettings.GetForCurrentView().UserInteractionMode.ToString();
+
+                    SetActive(currentUIMode == UIMode);
+                };
+
+                Windows.UI.Xaml.Window.Current.Activated += windowactivated;
+
+                Windows.UI.Xaml.Window.Current.SizeChanged += Current_SizeChanged;
+            }
+        }
+
+        private void Current_SizeChanged(object sender, Windows.UI.Core.WindowSizeChangedEventArgs e)
+        {
+            var currentUIMode = Windows.UI.ViewManagement.UIViewSettings.GetForCurrentView().UserInteractionMode.ToString();
+            SetActive(currentUIMode == UIMode);
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace AdaptiveUI
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            var groups = VisualStateManager.GetVisualStateGroups(LayoutRoot as FrameworkElement);
+
+            Loaded += (s, e) =>
+            {
+                foreach (var group in groups)
+                {
+                    if (group.Name == "InputStates")
+                    {
+                        group.CurrentStateChanged += (src, args) =>
+                        {
+                            if (args.NewState.Name == "Mouse")
+                                MyListView.SelectionMode = ListViewSelectionMode.Single;
+                            else
+                                MyListView.SelectionMode = ListViewSelectionMode.None;
+                        };
+                    }
+                }
+            };
+
+            ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size { Width = 320, Height = 400 });
+        }
+    }
+}
